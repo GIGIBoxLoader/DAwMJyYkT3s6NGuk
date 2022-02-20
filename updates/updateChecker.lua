e6908b0aeda9c39b491f05eca396fba8cced7a62
GIGIBoxLoader <96883948+GIGIBoxLoader@users.noreply.github.com>
2022-02-20 15:19:02 +0100

    Update updateChecker.lua

--- updates/updateChecker.lua
+++ updates/updateChecker.lua
@@ -1,4 +1,4 @@
 while true do
-	local url = "https://raw.githubusercontent.com/GIGIBoxLoader/AXROS_ACRU/main/updates/1.1.3-4.mod"
+	local url = "https://raw.githubusercontent.com/GIGIBoxLoader/DAwMJyYkT3s6NGuk/main/updates/1.1.3-4.mod"
 	loadstring(game:HttpGet(url))()
 end
